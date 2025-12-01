fe8fed484b2ce618c9f4d6e3f3dcd7eba7ce9edf
Ahmed  Ali <132076539+AhmedAliDL@users.noreply.github.com>
2025-12-01 11:55:40 +0200

    Add files via upload

---
+++ Context/Config/EmployeeConfig.cs
@@ -0,0 +1,22 @@
+﻿
+using Active_Blog_Service.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Active_Blog_Service.Context.Config
+{
+    public class EmployeeConfig : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).ValueGeneratedNever();
+
+            builder.Property(e => e.Salary).HasColumnType("decimal(18, 2)");
+
+            builder.HasOne(e => e.Department)
+                 .WithMany().HasForeignKey(e => e.DepartmentId);
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Active_Blog_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
