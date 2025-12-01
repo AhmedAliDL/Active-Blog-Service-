03e0d718434c4b011bd91fead45bfc25cf1ce878
Ahmed  Ali <132076539+AhmedAliDL@users.noreply.github.com>
2025-12-01 11:41:57 +0200

    Add files via upload

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Active_Blog_Service.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
